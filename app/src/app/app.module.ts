423f09f5695ddee823e99db4bec8152850b03cd9
wolfy <halfcrazypopcorn@gmail.com>
2023-03-23 15:34:26 +0100

    Initial

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { GridComponent } from './grid/grid.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DualComponent } from './dual/dual.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
+import { InfoGridComponent } from './info-grid/info-grid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    HomeComponent,
+    DetailsComponent,
+    GridComponent,
+    DualComponent,
+    InfoBoxComponent,
+    InfoGridComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ app/src/app/info-grid/info-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-grid',
+  templateUrl: './info-grid.component.html',
+  styleUrls: ['./info-grid.component.scss']
+})
+export class InfoGridComponent {
+
+}
